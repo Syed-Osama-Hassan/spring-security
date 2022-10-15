710358db0ef905abce53e2de5c6f191b5486cf28
syed-osama-hassan <syedosamahassan16@gmail.com>
2022-10-15 18:08:06 +0500

    Added UserDetailsService to create custom user with password encoder

--- src/main/java/com/syed/osama/hassan/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/syed/osama/hassan/springsecurity/security/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.syed.osama.hassan.springsecurity.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class Config {
+@AllArgsConstructor
+public class SecurityConfig {
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,6 +32,17 @@ public class Config {
                 .httpBasic();
 
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("John")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
