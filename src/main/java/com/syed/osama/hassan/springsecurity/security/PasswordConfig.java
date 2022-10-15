071a43b323d9f579aacb7b56cac7a2b9c48e1685
syed-osama-hassan <syedosamahassan16@gmail.com>
2022-10-15 18:10:09 +0500

    Code refactor

--- src/main/java/com/syed/osama/hassan/springsecurity/security/PasswordConfig.java
+++ src/main/java/com/syed/osama/hassan/springsecurity/security/PasswordConfig.java
@@ -1,5 +1,6 @@
 package com.syed.osama.hassan.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,9 +9,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordConfig {
 
+    @Value("10")
+    public int STRENGTH;
+
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+        return new BCryptPasswordEncoder(STRENGTH);
     }
 
 }
