7d2f1c2fac2147cf348483d3b3b4e9c3dc26f37d
syed-osama-hassan <syedosamahassan16@gmail.com>
2022-10-15 16:59:34 +0500

    Added project setup

---
+++ src/test/java/com/syed/osama/hassan/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.osama.hassan.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
